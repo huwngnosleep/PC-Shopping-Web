629d782b814b5ba9888773e0dc7e58bfe9f5102f
huwngnosleep <thanhhung8210@gmail.com>
2021-01-22 22:58:18 +0700

    updated something

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -76,15 +76,15 @@ class ProductsCategories extends Component {
   constructor() {
     super();
     this.state = {
-      categories: []
+      categories: [
+        {"title": "PC", "img": "https://phongvu.vn/media/wysiwyg/Hoang/uat/Case.jpg"},
+        {"title": "Laptop", "img": "https://phongvu.vn/media/wysiwyg/Hoang/CateBL/T112020/Hp.jpg"},
+        {"title": "Screen", "img": "https://phongvu.vn/media/wysiwyg/Hoang/CateBL/122020/unnamed.jpg"},
+        {"title": "Accessory", "img": "https://lh3.googleusercontent.com/hOKClaqdNQtQfwuDFEWc5uOZvZRJtegRXu9H9X4PnWKEe0zU1rPQHJdKOaJq96rmCMG7XfsTayBHlyPaTyfp=w500-rw-rw"},
+        {"title": "Gear", "img": "https://lh3.googleusercontent.com/11qFIIJkk4A9Oaa4DZCGHrKxmBJkE38B0JXwhp4JRAxwso4A4oEDhzPv1JYq8ZrCUs4qg8gNT7eKMCxWEoQ=w500-rw"},
+        {"title": "Smart Watch", "img": "https://phongvu.vn/media/wysiwyg/Hoang/CateBL/122020/applese.jpg"}
+      ],
     }
-  }
-  componentDidMount() {
-    axios.get('https://mg5em.sse.codesandbox.io/categories').then(res =>
-      this.setState({
-        categories: res.data
-      })
-    )
   }
 
   render() {
@@ -118,21 +118,136 @@ class BestSeller extends Component {
     super();
     this.state = {
       page: 0,
-      popularBrands: [],
-      bestSellers: [],
+      popularBrands: [
+        {"name": "MSI", "img": "https://lh3.googleusercontent.com/BdoRaaplaDNYSYRbYGinL43LaoWGrnansdiMDgawSksBM7NxlERC0SoVgvUh4H8rRnUCVHVNOxGvD7flVmNh=rw"},
+        {"name": "HP", "img": "https://lh3.googleusercontent.com/sgQpul0smf3zXy4JOgD7OLzhadi_PbhS2FzocH5rv8rayiKyREPBgCAmlZE_YWuXTb6S1D5kExErV6PCOQ=rw"},
+        {"name": "LG", "img": "https://lh3.googleusercontent.com/l2y1xkUV5p_aRjxyiqIQ9yyvj9vqukSKf2A2l3vd3tcnU9FefzFCScndOuyWil3w98rG618FEsz-t-moEYeI=rw"},
+        {"name": "Lenovo", "img": "https://lh3.googleusercontent.com/w9mrW6_W7yVglkXPNlhRTXvOzl02Fazc1425qJBzrD2oxQ9Zk0Qwi7pOLjUr9ws8YUB5EC9ooNi8XXD94hc=rw"}
+      ],
+      bestSellers: [
+        {
+          "name": "Laptop ASUS VivoBook 14 A412FA-EK734T (14 FHD/i5-10210U/8GB/512GB SSD/Intel UHD/Win10/1.5kg)",
+          "price": "$988.49",
+          "category": "laptop",
+          "productor": "Asus",
+          "img": "https://lh3.googleusercontent.com/6tvXgbEGh1b43wyxkxoVfNZ3YfKYl9AQC61n_1P_zZ1QtrLZLdi-QFdbDdplZfUfPPdu0Ek47h3rjlOkdw=w500-rw",
+          "info": ["CPU Intel Core i5-9400F (9M Cache, up to 4.10GHz)",  
+            "Mainboard Gigabyte B360M Aorus Gaming 3",
+            "RAM Gigabyte AORUS RGB 16GB (2x8) DDR4 3200 (GP-AR32C16S8K2HU416R)",
+            "Card Gigabyte GeForce GTX 1660Ti OC 6G GDDR6 (GV-N166TOC-6GD)",
+            "SSD Gigabyte 2.5 120GB SATA 6Gb/s (GP-GSTFS31120GNTD)",
+            "Power Gigabyte 500W (GP-PB500)",
+            "Case Gigabyte AORUS AC300W LITE",
+            "Quạt CPU Gigabyte Aorus ATC700"
+          ]
+        }, {
+          "name": "Laptop Dell G3 15 3590-N5I5518W (15 FHD/i5-9300H/8GB/512GB SSD/GTX 1650/Win10/2.3kg)",
+          "price": "$567.81",
+          "category": "laptop",
+          "productor": "Asus",
+          "img": "https://lh3.googleusercontent.com/H44s7IPyy-jSqjdujA89L5Al0ajMb43PpCHs1AzboIK_KueRRX_DJVVc4b8bfzaCI8Utux-wHHCiMkvXJqo=w500-rw",
+          "info": ["CPU Intel Core i5-9400F (9M Cache, up to 4.10GHz)",  
+            "Mainboard Gigabyte B360M Aorus Gaming 3",
+            "RAM Gigabyte AORUS RGB 16GB (2x8) DDR4 3200 (GP-AR32C16S8K2HU416R)",
+            "Card Gigabyte GeForce GTX 1660Ti OC 6G GDDR6 (GV-N166TOC-6GD)",
+            "SSD Gigabyte 2.5 120GB SATA 6Gb/s (GP-GSTFS31120GNTD)",
+            "Power Gigabyte 500W (GP-PB500)",
+            "Case Gigabyte AORUS AC300W LITE",
+            "Quạt CPU Gigabyte Aorus ATC700"
+          ]
+        }, {
+          "name": "Laptop HP 15s-fq1022TU (8VY75PA) (15 FHD/i7-1065G7/8GB/512GB SSD/Intel UHD/Win10/1.7kg)",
+          "price": "$153.51",
+          "category": "laptop",
+          "productor": "HP",
+          "img": "https://lh3.googleusercontent.com/ENWQOMkH5y5_BavnmrLQcrdu8pYLGd57H9upthP1SXHYEfgoa7YQtfMsDUzk6nYe9LPwTImHVRUXZQBStk8=w500-rw",
+          "info": ["CPU Intel Core i5-9400F (9M Cache, up to 4.10GHz)",  
+            "Mainboard Gigabyte B360M Aorus Gaming 3",
+            "RAM Gigabyte AORUS RGB 16GB (2x8) DDR4 3200 (GP-AR32C16S8K2HU416R)",
+            "Card Gigabyte GeForce GTX 1660Ti OC 6G GDDR6 (GV-N166TOC-6GD)",
+            "SSD Gigabyte 2.5 120GB SATA 6Gb/s (GP-GSTFS31120GNTD)",
+            "Power Gigabyte 500W (GP-PB500)",
+            "Case Gigabyte AORUS AC300W LITE",
+            "Quạt CPU Gigabyte Aorus ATC700"
+          ]
+        }, {
+          "name": "CPU Intel Core i5-9400F (6C/6T, 2.9 - 4.1 GHz, 9MB) - LGA 1151-v2",
+          "price": "$199.53",
+          "category": "accessory",
+          "productor": "Intel",
+          "img": "https://lh3.googleusercontent.com/WkEFbZM02MrWi2xg_oYUV9wcU24tQoRwRaT4nZmB6Jen1HLD23PZhqkAed_eturDck_kstljO3B7l9H3fjaY=w500-rw",
+          "info": ["CPU Intel Core i5-9400F (9M Cache, up to 4.10GHz)",  
+            "Mainboard Gigabyte B360M Aorus Gaming 3",
+            "RAM Gigabyte AORUS RGB 16GB (2x8) DDR4 3200 (GP-AR32C16S8K2HU416R)",
+            "Card Gigabyte GeForce GTX 1660Ti OC 6G GDDR6 (GV-N166TOC-6GD)",
+            "SSD Gigabyte 2.5 120GB SATA 6Gb/s (GP-GSTFS31120GNTD)",
+            "Power Gigabyte 500W (GP-PB500)",
+            "Case Gigabyte AORUS AC300W LITE",
+            "Quạt CPU Gigabyte Aorus ATC700"
+          ]
+        }, {
+          "name": "Card MSI GeForce GTX 1050Ti 4GB GDDR5 OCV1 (GTX-1050-Ti-4GT-OCV1)",
+          "price": "$630.61",
+          "category": "accessory",
+          "productor": "MSI",
+          "img": "https://lh3.googleusercontent.com/osxjiLndX72N5TlsaE5VC5jkOx4Oa0ZtPARtPU1KWqNMzY_6h-jWVSJUur881SQxqInijzjRBxZaZtYXEBiV0MQGRx4P2VCH=w500-rw",
+          "info": ["CPU Intel Core i5-9400F (9M Cache, up to 4.10GHz)",  
+            "Mainboard Gigabyte B360M Aorus Gaming 3",
+            "RAM Gigabyte AORUS RGB 16GB (2x8) DDR4 3200 (GP-AR32C16S8K2HU416R)",
+            "Card Gigabyte GeForce GTX 1660Ti OC 6G GDDR6 (GV-N166TOC-6GD)",
+            "SSD Gigabyte 2.5 120GB SATA 6Gb/s (GP-GSTFS31120GNTD)",
+            "Power Gigabyte 500W (GP-PB500)",
+            "Case Gigabyte AORUS AC300W LITE",
+            "Quạt CPU Gigabyte Aorus ATC700"
+          ]
+        }, {
+          "name": "Laptop ASUS TUF Gaming FX505DD-AL186T (15 FHD/R5-3550H/8GB/512GB SSD/GTX 1050/Win10/2.2 kg)",
+          "price": "$444.81",
+          "category": "laptop",
+          "productor": "Asus",
+          "img": "https://lh3.googleusercontent.com/LS1RIZ19enH0xMwF7L4x3h3zVK7duUueQjjUZb-7sRc3moNN4v3GmLPDlYr0v83a2jvUZczB-VkK-w_cAS4=w500-rw",
+          "info": ["CPU Intel Core i5-9400F (9M Cache, up to 4.10GHz)",  
+            "Mainboard Gigabyte B360M Aorus Gaming 3",
+            "RAM Gigabyte AORUS RGB 16GB (2x8) DDR4 3200 (GP-AR32C16S8K2HU416R)",
+            "Card Gigabyte GeForce GTX 1660Ti OC 6G GDDR6 (GV-N166TOC-6GD)",
+            "SSD Gigabyte 2.5 120GB SATA 6Gb/s (GP-GSTFS31120GNTD)",
+            "Power Gigabyte 500W (GP-PB500)",
+            "Case Gigabyte AORUS AC300W LITE",
+            "Quạt CPU Gigabyte Aorus ATC700"
+          ]
+        }, {
+          "name": "LG 27 27MP59G-P (1920x1080/IPS/75Hz/5ms/FreeSync)",
+          "price": "$163.44",
+          "category": "screen",
+          "productor": "Intel",
+          "img": "https://lh3.googleusercontent.com/VqH0A1o-ESpFUxOGeVc-0B09aWSNrj_NeatFasP1dv_feKBwZQo8JCFQO7HgCOvy5-hXKOPsKkBT6ieckq8=w500-rw",
+          "info": ["CPU Intel Core i5-9400F (9M Cache, up to 4.10GHz)",  
+            "Mainboard Gigabyte B360M Aorus Gaming 3",
+            "RAM Gigabyte AORUS RGB 16GB (2x8) DDR4 3200 (GP-AR32C16S8K2HU416R)",
+            "Card Gigabyte GeForce GTX 1660Ti OC 6G GDDR6 (GV-N166TOC-6GD)",
+            "SSD Gigabyte 2.5 120GB SATA 6Gb/s (GP-GSTFS31120GNTD)",
+            "Power Gigabyte 500W (GP-PB500)",
+            "Case Gigabyte AORUS AC300W LITE",
+            "Quạt CPU Gigabyte Aorus ATC700"
+          ]
+        }, {
+          "name": "LCD MSI Optix G27C4 (1920 x 1080/VA/165Hz/1 ms/FreeSync)",
+          "price": "$749.91",
+          "category": "screen",
+          "productor": "MSI",
+          "img": "https://lh3.googleusercontent.com/7rNEDXiEw1hZ879YRJSIA5bNgFpQ8qQYh_RWYHP5UDTB1xHcu5n6gm__tYAnkEyBT_Yj0vbcky46A0e5e-g=w500-rw",
+          "info": ["CPU Intel Core i5-9400F (9M Cache, up to 4.10GHz)",  
+            "Mainboard Gigabyte B360M Aorus Gaming 3",
+            "RAM Gigabyte AORUS RGB 16GB (2x8) DDR4 3200 (GP-AR32C16S8K2HU416R)",
+            "Card Gigabyte GeForce GTX 1660Ti OC 6G GDDR6 (GV-N166TOC-6GD)",
+            "SSD Gigabyte 2.5 120GB SATA 6Gb/s (GP-GSTFS31120GNTD)",
+            "Power Gigabyte 500W (GP-PB500)",
+            "Case Gigabyte AORUS AC300W LITE",
+            "Quạt CPU Gigabyte Aorus ATC700"
+          ]
+        }
+      ],
     }
-  }
-  componentDidMount() {
-    axios.get('https://mg5em.sse.codesandbox.io/popularBrands').then(res => 
-      this.setState({
-        popularBrands: res.data
-      })
-    )
-    axios.get('https://mg5em.sse.codesandbox.io/bestSellers').then(res =>
-      this.setState({
-        bestSellers: res.data
-      })
-    )
   }
 
   leftArrowClick() {
